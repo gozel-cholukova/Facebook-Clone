51324128e4cc4e8bc8fca9531c91f1bde2181e7f
Gozel Cholukova <gozelrahmedova@gmail.com>
2020-12-15 21:19:39 +0500

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyA13IdWNSB_z7IuGzBTK1cFrcTOuMejjMc",
+  authDomain: "instagram-clone-react-8f859.firebaseapp.com",
+  databaseURL: "https://instagram-clone-react-8f859-default-rtdb.firebaseio.com",
+  projectId: "instagram-clone-react-8f859",
+  storageBucket: "instagram-clone-react-8f859.appspot.com",
+  messagingSenderId: "844871740921",
+  appId: "1:844871740921:web:e3aab7349181b00d2c2128",
+  measurementId: "G-9KX37Q4EPL"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
+
+
+// export default db;
