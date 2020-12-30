5c4be0e76ca2dfca7b9a4e83b9e47678e57a2241
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-31 03:16:33 +0500

    Add files via upload

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Feed.css";
 import StoryReel from "./StoryReel"
+import MessageSender from "./MessageSender"
 
 function Feed() {
   return (
@@ -8,7 +9,8 @@ function Feed() {
       {/* StoryReel */}
       <StoryReel />
       {/* MessageSender */}
+      <MessageSender />
     </div>
-  )
+  );
 }
 export default Feed;
