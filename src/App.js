4e02e19bc895c87839d0c874b49f4b2cc1e813f5
Gozel Cholukova <gozelr@mail.ru>
2021-01-04 03:59:34 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,28 +4,35 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
+import Login from "./Login";
+import firebase from "firebase";
 
-// import firebase from './firebase';
 
 function App() {
+  const user = null;
   return (
     //BEM naming convention
     <div className="app">
-      {/* Header */}
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          {/* Header */}
+          <Header />
 
-      {/* App body */}
-      <div className="app__body">
-      {/* Sidebar */}
-      <Sidebar />
+          {/* App body */}
+          <div className="app__body">
+          {/* Sidebar */}
+          <Sidebar />
 
-      {/* Feed */}
-      <Feed />
-      
-      {/* Widgets */}
-      <Widgets />
-      </div>     
-      
+          {/* Feed */}
+          <Feed />
+          
+          {/* Widgets */}
+          <Widgets />
+          </div> 
+        </>
+      )}
     </div>
   );
 }
